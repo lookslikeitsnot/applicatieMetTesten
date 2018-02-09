e18cb233331b7fdc91b427f8f8038f1ada2d4fe7
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-02-09 12:13:30 +0100

    met before

--- src/test/java/be/vdab/valueobjects/RekeningTest.java
+++ src/test/java/be/vdab/valueobjects/RekeningTest.java
@@ -4,18 +4,24 @@ import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RekeningTest {
+	private Rekening rekening;
+
+	@Before
+	public void before() {
+		rekening = new Rekening();
+	}
+
 	@Test
 	public void hetSaldoVanEenNieuweRekeningIsNul() {
-		Rekening rekening = new Rekening();
 		assertEquals(0, BigDecimal.ZERO.compareTo(rekening.getSaldo()));
 	}
 
 	@Test
 	public void hetSaldoNaEenEersteStortingIsHetBedragVanDieStorting() {
-		Rekening rekening = new Rekening();
 		BigDecimal bedrag = BigDecimal.valueOf(2.5);
 		rekening.storten(bedrag);
 		assertEquals(0, bedrag.compareTo(rekening.getSaldo()));
@@ -23,7 +29,6 @@ public class RekeningTest {
 
 	@Test
 	public void hetSaldoNaTweeStortingenIsDeSomVanDeBedragenVanDieStortingen() {
-		Rekening rekening = new Rekening();
 		rekening.storten(BigDecimal.valueOf(2.5));
 		rekening.storten(BigDecimal.valueOf(1.2));
 		assertEquals(0, BigDecimal.valueOf(3.7).compareTo(rekening.getSaldo()));
