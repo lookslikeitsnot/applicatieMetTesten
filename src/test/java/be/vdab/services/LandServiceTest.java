6c9d4f026a9635b10ef3af9694e2f81dda63956a
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-02-12 13:19:39 +0100

    met Mockito

--- src/test/java/be/vdab/services/LandServiceTest.java
+++ src/test/java/be/vdab/services/LandServiceTest.java
@@ -1,14 +1,17 @@
 package be.vdab.services;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 import java.math.BigDecimal;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import be.vdab.entities.Land;
 import be.vdab.repositories.LandRepository;
-import be.vdab.repositories.LandRepositoryStub;
 
 public class LandServiceTest {
 	private LandRepository landRepository;
@@ -16,7 +19,11 @@ public class LandServiceTest {
 
 	@Before
 	public void before() {
-		landRepository = new LandRepositoryStub();
+		landRepository = mock(LandRepository.class);
+		when(landRepository.findOppervlakteAlleLanden()).thenReturn(20);
+		when(landRepository.read("B")).thenReturn(new Land("B", 5));
+		when(landRepository.read("NL")).thenReturn(new Land("NL", 6));
+		when(landRepository.read("")).thenThrow(new IllegalArgumentException());
 		landService = new LandService(landRepository);
 	}
 
@@ -24,5 +31,7 @@ public class LandServiceTest {
 	public void findVerhoudingOppervlakteLandTovOppervlakteAlleLanden() {
 		assertEquals(0, BigDecimal.valueOf(0.25)
 				.compareTo(landService.findVerhoudingOppervlakteLandTovOppervlakteAlleLanden("B")));
+		verify(landRepository).findOppervlakteAlleLanden();
+		verify(landRepository).read("B");
 	}
 }
